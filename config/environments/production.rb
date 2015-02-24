1e00ffaa414875a5ffb80e39316377b0188bd763
sawhney-kartik <sawhney.kartik@gmail.com>
2015-02-24 03:16:00 -0800

    test2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-MovieHub::Application.configure do
+AccessibleLibrary::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -20,7 +20,7 @@ MovieHub::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
